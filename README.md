eea02d0aeff50ebeea82cfcecda0b566b92aa8cc
ProfAlex2021 <80551988+ProfAlex2021@users.noreply.github.com>
2023-06-20 15:08:15 -0300

    Enunciado para atividade

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dsfatecpw2023
+
+Continuando o tutorial de menu sticky:
+https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+
+Continuar o código de CSS e substituir o conteúdo de HTML para o de sua preferência
--- menu.js
+++ menu.js
@@ -1,1 +1,13 @@
-var navbar = document.getElementById("navbar");
+window.onload = function () {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    window.onscroll = function() { myFunction(navbar, sticky) };
+}
+
+function myFunction(navbar, stickys){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
